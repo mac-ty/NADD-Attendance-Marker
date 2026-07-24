f555eac136f3dca345cfc63d7cecddeeae05cf66
Mac Ty <esenetheodoremacaulay3@gmail.com>
2026-07-24 00:37:05 +0000

    Change the API_BASE url to point to either render's URL or localhost

--- frontend/lecturer.js
+++ frontend/lecturer.js
@@ -1,5 +1,7 @@
 
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
+    ? "http://localhost:8080/api"
+    : "https://nadd-attendance-marker.onrender.com/api";
 let token = null;
 
 const signUpForm = document.getElementById("sign-up-form");
